88c72f885ffa5045943848e3301a302a1deb3cfb
Serhan Elmalı <serhanelmali@gmail.com>
2022-01-29 21:31:49 +0300

    adds Transaction class

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Transaction {
+    constructor(amount, payer, //public key
+    payee //public key
+    ) {
+        this.amount = amount;
+        this.payer = payer;
+        this.payee = payee;
+    }
+    toString() {
+        return JSON.stringify(this);
+    }
+}
---
+++ index.ts
@@ -0,0 +1,13 @@
+import * as crypto from "crypto"
+
+class Transaction {
+  constructor(
+    public amount: number,
+    public payer: string, //public key
+    public payee: string //public key
+  ) {}
+
+  toString() {
+    return JSON.stringify(this)
+  }
+}
