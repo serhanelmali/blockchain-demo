59c74786f73b8324b1b7927df1f6fe14e7793337
Serhan Elmalı <serhanelmali@gmail.com>
2022-01-29 21:49:06 +0300

    adds Wallet Class

--- index.js
+++ index.js
@@ -58,3 +58,21 @@ class Chain {
     }
 }
 Chain.instance = new Chain();
+class Wallet {
+    constructor() {
+        const keyPair = crypto.generateKeyPairSync("rsa", {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: "spki", format: "pem" },
+            privateKeyEncoding: { type: "pkcs8", format: "pem" },
+        });
+        this.publicKey = keyPair.publicKey;
+        this.privateKey = keyPair.privateKey;
+    }
+    sendMoney(amount, payeePublicKey) {
+        const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+        const sign = crypto.createSign("SHA256");
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey);
+        Chain.instance.addBlock(transaction, this.publicKey, signature);
+    }
+}
--- index.ts
+++ index.ts
@@ -44,9 +44,35 @@ class Chain {
   addBlock(
     transaction: Transaction,
     senderPublicKey: string,
-    signature: string
+    signature: Buffer
   ) {
     const newBlock = new Block(this.lastBlock.hash, transaction)
     this.chain.push(newBlock)
+  }
+}
+
+class Wallet {
+  public publicKey: string
+  public privateKey: string
+
+  constructor() {
+    const keyPair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    })
+
+    this.publicKey = keyPair.publicKey
+    this.privateKey = keyPair.privateKey
+  }
+
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey)
+
+    const sign = crypto.createSign("SHA256")
+    sign.update(transaction.toString()).end()
+
+    const signature = sign.sign(this.privateKey)
+    Chain.instance.addBlock(transaction, this.publicKey, signature)
   }
 }
